c44383b32df81f04ed18fd1ad1c17ee2ef907820
pheadlessg <seankfisher@gmail.com>
2022-05-02 11:26:12 -0700

    added acting reel to demos

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -3,12 +3,23 @@ import React from 'react';
 const Demos = () => {
   return (
     <div>
-      <p className="pageheader">Commercial Demos</p>
+      <p className="pageheader">Commercial Voiceover Demo</p>
       <iframe
         className="soundcloud"
         title="soundcloud"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/766979650&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
       />
+      <p className="pageheader">Performance Reel</p>
+      <iframe
+        className="soundcloud"
+        title="soundcloud"
+        src="https://player.vimeo.com/video/691058644?h=1a8474fabf"
+        width="640"
+        height="360"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
